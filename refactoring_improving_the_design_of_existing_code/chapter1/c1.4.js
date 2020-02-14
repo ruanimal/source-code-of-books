c0b9998e65af689a082c56ee8502227a43ece215
ruan.lj <ruan.lj@foxmail.com>
2020-02-14 15:14:41 +0800

    M  chapter1/c1.4.js

--- refactoring_improving_the_design_of_existing_code/chapter1/c1.4.js
+++ refactoring_improving_the_design_of_existing_code/chapter1/c1.4.js
@@ -65,6 +65,14 @@ function usd(aNumber) {
                           minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
+function totalVolumeCredits(invoice) {
+  let result = 0
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf)
+  }
+  return result
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -73,12 +81,8 @@ function statement (invoice, plays) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf)
-  }
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
   return result;
 }
 
