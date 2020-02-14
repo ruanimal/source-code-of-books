02e6def78239d70d7671024714e2ae5746d4d19c
ruan.lj <ruan.lj@foxmail.com>
2020-02-14 17:30:14 +0800

    M  chapter1/c1.6.js

--- refactoring_improving_the_design_of_existing_code/chapter1/c1.6.js
+++ refactoring_improving_the_design_of_existing_code/chapter1/c1.6.js
@@ -29,6 +29,10 @@ function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays))
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function createStatementData(invoice, plays) {
   const result =  {}
   result.customer = invoice.customer
@@ -93,12 +97,27 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-                          { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2 }).format(aNumber/100);
+function renderHtml (data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+                        { style: "currency", currency: "USD",
+                          minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
 console.log(statement(invoices[0], plays))
+console.log(htmlStatement(invoices[0], plays))
