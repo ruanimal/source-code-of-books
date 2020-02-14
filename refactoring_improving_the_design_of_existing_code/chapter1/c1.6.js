f57e136b8f6a5b0fc50101e683574085f57009cc
ruan.lj <ruan.lj@foxmail.com>
2020-02-14 17:10:41 +0800

    M  chapter1/c1.6.js

--- refactoring_improving_the_design_of_existing_code/chapter1/c1.6.js
+++ refactoring_improving_the_design_of_existing_code/chapter1/c1.6.js
@@ -34,10 +34,32 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result, plays)
+    result.amount = amountFor(result)
     return result
 
     function playFor(aPerformance, plays) {
       return plays[aPerformance.playID]
+    }
+    function amountFor(aPerformance) {
+      let result = 0;
+      switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+        default:
+            throw new Error(`unknown type: ${aPerformance.play.type}`);
+      }
+      return result
     }
   }
 }
@@ -45,34 +67,13 @@ function statement(invoice, plays) {
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
   let totalAmount = appleSauce(data)
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits(data)} credits\n`;
   return result;
 
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-      default:
-          throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result
-  }
   function volumeCreditsFor(perf) {
     let result = 0;
     result += Math.max(perf.audience - 30, 0);
@@ -96,7 +97,7 @@ function renderPlainText(data) {
   function appleSauce(invoice) {
     let result = 0;
     for (let perf of invoice.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result
   }
