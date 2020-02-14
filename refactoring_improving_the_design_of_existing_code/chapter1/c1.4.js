1aeb2346cde312e55ff58d3c2d9cd5115a0e1a84
ruan.lj <ruan.lj@foxmail.com>
2020-02-14 15:04:55 +0800

    M  chapter1/c1.4.js

--- refactoring_improving_the_design_of_existing_code/chapter1/c1.4.js
+++ refactoring_improving_the_design_of_existing_code/chapter1/c1.4.js
@@ -59,10 +59,10 @@ function volumeCreditsFor(perf) {
   return result
 }
 
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
                         { style: "currency", currency: "USD",
-                          minimumFractionDigits: 2 }).format(aNumber);
+                          minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
 function statement (invoice, plays) {
@@ -70,12 +70,12 @@ function statement (invoice, plays) {
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-      volumeCredits = volumeCreditsFor(perf)
+      volumeCredits += volumeCreditsFor(perf)
       // print line for this order
-      result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+      result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
