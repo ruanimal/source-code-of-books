5a013ea178b4c426295c6b78969c0bf32f70a22c
ruan.lj <ruan.lj@foxmail.com>
2020-02-14 14:42:33 +0800

    M  chapter1/c1.4.js

--- refactoring_improving_the_design_of_existing_code/chapter1/c1.4.js
+++ refactoring_improving_the_design_of_existing_code/chapter1/c1.4.js
@@ -28,9 +28,9 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID]
 }
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
   let result = 0;
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
   case "tragedy":
     result = 40000;
     if (aPerformance.audience > 30) {
@@ -45,7 +45,7 @@ function amountFor(aPerformance, play) {
     result += 300 * aPerformance.audience;
     break;
     default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
   }
   return result
 }
@@ -58,8 +58,7 @@ function statement (invoice, plays) {
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-      // const playFor(perf) = playFor(perf)
-      let thisAmount = amountFor(perf, playFor(perf))
+      let thisAmount = amountFor(perf)
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
