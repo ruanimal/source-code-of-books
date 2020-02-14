a34a5ed3a7eb07780833eacd0116a22d39046f71
ruan.lj <ruan.lj@foxmail.com>
2020-02-14 17:22:33 +0800

    M  chapter1/c1.6.js

--- refactoring_improving_the_design_of_existing_code/chapter1/c1.6.js
+++ refactoring_improving_the_design_of_existing_code/chapter1/c1.6.js
@@ -73,19 +73,11 @@ function statement(invoice, plays) {
       return result
     }
   }
-  function totalVolumeCredits(invoice) {
-    let result = 0
-    for (let perf of invoice.performances) {
-      result += perf.volumeCredits
-    }
-    return result
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
   }
-  function totalAmount(invoice) {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += perf.amount;
-    }
-    return result
+  function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.amount, 0)
   }
 }
 
