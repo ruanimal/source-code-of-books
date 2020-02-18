b0995bf377990a4a96b812abdaeb6078e5f428c2
ruan.lj <ruan.lj@foxmail.com>
2020-02-18 15:49:01 +0800

    M  chapter1/c1.8/createStatementData.js

--- refactoring_improving_the_design_of_existing_code/chapter1/c1.8/createStatementData.js
+++ refactoring_improving_the_design_of_existing_code/chapter1/c1.8/createStatementData.js
@@ -9,12 +9,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performances.audience - 30, 0);
-    if ("comedy" === this.play.type) {
-      result += Math.floor(this.performances.audience / 5);
-    }
-    return result
+    return Math.max(this.performances.audience - 30, 0);
   }
 }
 
@@ -36,6 +31,10 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performances.audience;
     return result
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performances.audience / 5);
   }
 }
 
